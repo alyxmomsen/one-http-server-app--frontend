961aaa9a7ca43a4bec3de4a2750fb8c8513f50ea
alex <shizofranky@gmail.com>
2025-04-01 00:38:39 +0300

    update

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,20 +2,20 @@ export class MyClass {
 
     private defaultBehavior:IBehavior;
 
-    private request(clickBehavior:IBehavior|null = null) {
+    private request(requestBehavior:IBehavior|null = null) {
 
-        if(clickBehavior === null) return this.defaultBehavior.exec();
+        if(requestBehavior === null) return this.defaultBehavior.exec();
         
-        clickBehavior.exec();
+        requestBehavior.exec();
 
     }
 
-    doSomeThing(before:() => void , clickBehavior:IBehavior|null ,after:()=>void){
-        before();
+    doSomeThing(beforeRequest:() => void , requestBehavior:IBehavior|null ,afterResponse:()=>void){
+        beforeRequest();
 
-        this.request(clickBehavior);
+        this.request(requestBehavior);
         
-        after();
+        afterResponse();
         
     }
 
@@ -41,18 +41,13 @@ export abstract class Behavior implements IBehavior {
 
     protected requestData:TRequestData ;
 
-    exec(/* opcode:EnumHookOpCode */): string {
-
-        // const requestData:TRequestData = {
-        //     opcode,
-        // }
+    exec(): string {
 
         const baseurl  = "https://ardently-refreshing-opah.cloudpub.ru/" ;
         // const baseurl = "http://localhost:3000/" ;
 
-
         /**
-         * endpoint name is temp
+         * name of this endpoint is temporary 
          */
         fetch(baseurl + 'api/add-transaction' , {
             method:'post',
@@ -83,17 +78,17 @@ export abstract class Behavior implements IBehavior {
 
 export class Behavior_default extends Behavior {
     constructor () {
-        super({opcode:0});
+        super({opcode:EnumHookOpCode.add_transaction});
     }
 }
 export class Behavior_1 extends Behavior {
     constructor () {
-        super({opcode:0});
+        super({opcode:EnumHookOpCode.add_transaction});
     }
 }
 
 export class Behavior_2 extends Behavior {
     constructor () {
-        super({opcode:1});
+        super({opcode:EnumHookOpCode.other});
     }
 }
